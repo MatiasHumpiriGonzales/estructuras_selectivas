34ca4550c52efed0ea317e3975454d131852f8c7
Matias <mhumpirig@unjbg.edu.pe>
2025-05-29 21:18:41 -0500

    Agregar Ejercicio1.cpp

---
+++ Ejercicio1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int num1, num2, num3, promedio;
+	cout << "Indique el valor de la nota1 (del 0 al 100): ";
+	cin >> num1;
+	cout << "Indique el valor de la nota2 (del 0 al 100): ";
+	cin >> num2;
+	cout << "Indique el valor de la nota3 (del 0 al 100): ";
+	cin >> num3;
+	promedio = (num1 + num2 +num3) / 3;
+	if (promedio >= 70){
+		cout << "APROBADO";
+	}
+	else {
+		cout << "DESAPROBADO";
+	}
+	return 0;
+}
