ba0df05614bae8593415d9834640b421825d848c
Matias <mhumpirig@unjbg.edu.pe>
2025-05-31 12:58:11 -0500

    Ahora es el mayor de 4 numeros

--- Ejercicio9.cpp
+++ Ejercicio9.cpp
@@ -1,21 +1,26 @@
 #include <iostream>
 using namespace std;
 int main (){
-	int a, b, c, mayor;
+	int a, b, c, d, mayor;
 	cout << "Indique el valor del primer numero: ";
 	cin >> a;
 		cout << "Indique el valor del segundo numero: ";
 	cin >> b;
 		cout << "Indique el valor del tercer numero: ";
 	cin >> c;
-	if (a > b && a > c){
+	cout << "Indique el valor del cuarto numero: ";
+	cin >> d;
+	if (a > b && a > c && a > d){
 		mayor = a;
 	}
-	else if (b > a && b > c){
+	else if (b > a && b > c && b > d){
 		mayor = b;
 	}
-	else {
+	else if (c > a && c > b && c > d){
 		mayor = c;
+	}
+	else {
+		mayor = d;
 	}
 	cout << "El mayor numero es: " << mayor;
 	return 0;
