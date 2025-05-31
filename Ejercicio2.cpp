cb766a19d96dee5bc4a0a1841356f009ca848c8c
Matias <mhumpirig@unjbg.edu.pe>
2025-05-30 22:35:24 -0500

    Agregando Ejercicio2.cpp

---
+++ Ejercicio2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main (){
+	int n;
+	cout << "Ingrese el costo total: ";
+	cin >> n;
+	if (n > 1000){
+		n = n - (0.2 * n);
+			cout << "El costo final es: " << n + 1;
+	}
+	else { 
+		cout << "El costo final es: " << n;
+	}
+	return 0;
+}
