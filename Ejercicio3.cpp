ee3c07a13103388e9c816ba36ff1fd1015d5f065
Matias <mhumpirig@unjbg.edu.pe>
2025-06-01 10:26:19 -0500

    Ahora el descuento mayor es cuando compras 4 o más camisas

--- Ejercicio3.cpp
+++ Ejercicio3.cpp
@@ -6,10 +6,10 @@ int main() {
 	cin >> precio;
 	cout << "Indique la cantidad de camisas: ";
 	cin >> camisa;
-	if ( camisa >= 3){
+	if ( camisa >= 4){
 		descuento = precio - (0.2 * precio);
 	}
-	else if (camisa >= 0 && camisa < 3) {
+	else if (camisa >= 0 && camisa < 4) {
 		descuento = precio - (0.1 * precio);
 	}
 	cout << "El precio final sera: " << descuento + 1 << endl;
