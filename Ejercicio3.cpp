5b212003afceb1f48b8bef3cc4e03be4032e94c3
Matias <mhumpirig@unjbg.edu.pe>
2025-06-01 10:24:26 -0500

    Agregando Ejercicio3.cpp

---
+++ Ejercicio3.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int camisa, precio, descuento;
+	cout << "Indique el precio pagado inicialmente: ";
+	cin >> precio;
+	cout << "Indique la cantidad de camisas: ";
+	cin >> camisa;
+	if ( camisa >= 3){
+		descuento = precio - (0.2 * precio);
+	}
+	else if (camisa >= 0 && camisa < 3) {
+		descuento = precio - (0.1 * precio);
+	}
+	cout << "El precio final sera: " << descuento + 1 << endl;
+	return 0;
+}
